711489d1557e6ad87b6e7c9c3b1b458dc28d9e10
PierreSQS <nomo_messina@web.de>
2022-07-27 23:54:01 +0200

    Sect2_Chap231-Customize URL Path: added an assertion in test

--- src/test/java/guru/springframework/sfgrestbrewery/SfgRestfulBreweryApplicationTests.java
+++ src/test/java/guru/springframework/sfgrestbrewery/SfgRestfulBreweryApplicationTests.java
@@ -3,12 +3,16 @@ package guru.springframework.sfgrestbrewery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class SfgRestfulBreweryApplicationTests {
+class SfgRestfulBreweryApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
 	}
 
 }
